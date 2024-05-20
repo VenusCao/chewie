e1c86f5c2f352c574370f4f639b4b77f07dfce20
VenusCao <793710663@qq.com>
2024-05-20 15:14:39 +0800

    Update player_with_controls.dart
    fix

--- lib/src/player_with_controls.dart
+++ lib/src/player_with_controls.dart
@@ -44,8 +44,7 @@ class PlayerWithControls extends StatelessWidget {
             panEnabled: chewieController.zoomAndPan,
             scaleEnabled: chewieController.zoomAndPan,
             child:
-
-            chewieController.aspectRatio!=null?
+            chewieController.aspectRatio==null?
             Center(
               child: AspectRatio(
                 aspectRatio: chewieController.aspectRatio ??
@@ -100,78 +99,5 @@ class PlayerWithControls extends StatelessWidget {
         ),
       );
     });
-  }
-}
-
-
-class CroppedVideo extends StatefulWidget {
-  CroppedVideo({required this.controller,required this.cropAspectRatio});
-
-  final VideoPlayerController controller;
-  final double cropAspectRatio;
-
-  @override
-  CroppedVideoState createState() => CroppedVideoState();
-}
-
-class CroppedVideoState extends State<CroppedVideo> {
-  VideoPlayerController get controller => widget.controller;
-
-  double get cropAspectRatio => widget.cropAspectRatio;
-  bool initialized = false;
-
-  VoidCallback listener;
-
-  @override
-  void initState() {
-    super.initState();
-    _waitForInitialized();
-  }
-
-  @override
-  void didUpdateWidget(CroppedVideo oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.controller != controller) {
-      oldWidget.controller.removeListener(listener);
-      initialized = false;
-      _waitForInitialized();
-    }
-  }
-
-  void _waitForInitialized() {
-    listener = () {
-      if (!mounted) {
-        return;
-      }
-      if (initialized != controller.value.initialized) {
-        initialized = controller.value.initialized;
-        setState(() {});
-      }
-    };
-    controller.addListener(listener);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (initialized) {
-      return Center(
-        child: AspectRatio(
-          aspectRatio: cropAspectRatio ?? controller.value.aspectRatio,
-          child: FittedBox(
-            fit: BoxFit.cover,
-            child: SizedBox(
-              width: controller.value.size?.width ?? 0,
-              height: controller.value.size?.height ?? 0,
-              child: AspectRatio(
-                aspectRatio: controller.value.aspectRatio,
-                child: VideoPlayer(controller),
-              ),
-            ),
-          ),
-        ),
-      );
-    } else {
-      return Container();
-    }
   }
 }
